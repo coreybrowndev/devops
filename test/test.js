e9d420062f2fc724a5e063307171b6516df36aac
Corey Brown <bcorey395@gmail.com>
2024-11-11 16:50:47 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -8,13 +8,13 @@ describe('Regular fibonacci value', function() {
 
 describe('First two terms', function() {
   it('should return 0 and 1 for first two terms', function() {
-      assert.strictEqual(main.fibonacci(0), 0);
+      assert.strictEqual(main.factorial(0), 0);
       assert.strictEqual(main.fibonacci(1), 1);
     });
 });
 
 describe('negative numbers', function() {
   it('should return undefined', function() {
-    assert.strictEqual(main.fibonacci(-1), undefined)
+    assert.strictEqual(main.factorial(-1), undefined)
   })
 })

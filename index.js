68b112a27fc8236cac71ffc5abb26040845a1166
Corey Brown <bcorey395@gmail.com>
2024-11-11 17:02:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ exports.fibonacci = function(n) {
 
 
 exports.factorial = function (n) {
-    if (n < 0) {
-        return undefined
-    }
-    
+
+    if (n < 0) return undefined
+
     if (n == 0 || n == 1) {
         return 1
     }
 
-    return n * factorial(n - 1)
+    return n * this.factorial(n -  1)
 }
